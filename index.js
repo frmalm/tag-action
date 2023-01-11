e0eb2445054086e3d730f3677c9d79bd2819bfed
Fredrik Malm <fredrik.z.malm@teliacompany.com>
2023-01-11 11:56:26 +0100

    test

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const github = require('@actions/github');
 const main = async () => {
     try {
         const prefix = core.getInput('prefix', { required: true });
+        const major = core.getInput('major', { required: true });
         const token = core.getInput('token', { required: true });
 
         const octokit = new github.getOctokit(token);
 
-        octokit.log.info("hello")
+        var tag = new NewTag {
+                Tag = "v1.0.0",
+                Object = "ee062e0", // short SHA
+                Type = TaggedType.Commit, // TODO: what are the defaults when nothing specified?
+        };
 
-
+        Console.WriteLine("Created a tag for {0} at {1}", prefix, major);
 
     } catch (error) {
         core.setFailed(error.message);
